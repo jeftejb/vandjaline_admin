51352b3ac3eea52c0a391facb421b98c8ada9e93
jeftejb <jeftesambangojb@gmail.com>
2022-04-30 18:53:06 +0100

    my first commit

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,7 +1,7 @@
 import React from "react"
 import { useState } from "react"
 import {useDispatch} from "react-redux"
-import {loginUser, loginAdmin} from "../../redux/apiCalls"
+import {loginUser, loginAdmin} from "./../../redux/apiCalls"
 
 const Login = () => {
     const [nomeUsuario , setUserName] = useState("")
--- src/requestMetodos.js
+++ src/requestMetodos.js
@@ -4,21 +4,15 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 var TOKEN = ""
-
-if(localStorage.getItem("persist:vandja") !== null){
  if (JSON?.parse(JSON.parse(localStorage.getItem("persist:vandja"))?.user)?.currentUser){
-const res  =  JSON?.parse(JSON.parse(localStorage.getItem("persist:vandja"))?.user)?.currentUser?.acessoToken
-TOKEN = res
+ TOKEN  =  JSON?.parse(JSON.parse(localStorage.getItem("persist:vandja"))?.user)?.currentUser?.acessoTokenTOKEN 
 }else {
- const  res = JSON?.parse(JSON.parse(localStorage.getItem("persist:vandja"))?.lojaLogin)?.currentLoja?.acessoToken
- TOKEN = res
-}
-}else{
-    TOKEN = ""
+ TOKEN = JSON?.parse(JSON.parse(localStorage.getItem("persist:vandja"))?.lojaLogin)?.currentLoja?.acessoToken
+ 
 }
 
+const BASE_URL = process.env.REACT_APP_API_URL // "http://localhost:8080/api/"
 
-const BASE_URL = process.env.REACT_APP_API_LINK // "http://localhost:8080/api/"
 export const publicRequest = axios.create({
     baseURL:BASE_URL,
 })
