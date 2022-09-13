b55f08cc7703e4b13e26905cbc8d9a947e9e1f91
jeftejb <jeftesambangojb@gmail.com>
2022-09-13 11:08:36 +0100

    make it better

--- src/requestMetodos.js
+++ src/requestMetodos.js
@@ -13,7 +13,7 @@ if(localStorage.getItem("persist:vandja") ){
 }
 }
 
-//const BASE_URL =  'http://localhost:8080/api/'      
+//const BASE_URL = 'http://localhost:8080/api/'      
 const BASE_URL = process.env.REACT_APP_API_URL
 
 export const publicRequest = axios.create({
